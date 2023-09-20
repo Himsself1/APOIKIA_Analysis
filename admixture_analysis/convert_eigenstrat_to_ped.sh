97e240efd6c6c65aef8f69d7557f81b1322d8561
Himsself1 <spapadadonakis@gmail.com>
2023-09-20 23:57:45 +0300

    Added cross-validation with admixture32. Admixture 34 doesn't work

--- admixture_analysis/convert_eigenstrat_to_ped.sh
+++ admixture_analysis/convert_eigenstrat_to_ped.sh
@@ -1,1 +1,12 @@
-/usr/bin/convertf -p admixture_analysis/apoikia_to_plink.parfile
+#!/usr/bin/bash
+
+PATH_TO_INPUT="/home/aggeliki/apoikia/EIGENSTRAT/new_dataset_28_07"
+
+# * Converts eigenstrat to .ped
+/usr/bin/convertf -p ~/apoikia/APOIKIA_Analysis/admixture_analysis/apoikia_to_plink.parfile
+
+## ADMIXTURE doesn't like something about convertf output
+## PLINK doesn't seem to mind. Will convert to bed
+
+# * Call to PLINK for the conversion to bed and filtering
+plink1.9 --ped $PATH_TO_INPUT/apoikia.1240K.APOIKIA.ped --map $PATH_TO_INPUT/apoikia.1240K.APOIKIA.pedsnp --make-bed --geno 0.99 --out $PATH_TO_INPUT/apoikia.1240K.APOIKIA
--- admixture_analysis/script_for_admixture.sh
+++ admixture_analysis/script_for_admixture.sh
@@ -1,1 +1,18 @@
-$PATH_TO_DATA="~/apoikia_new_data/data"
+#!/usr/bin/bash
+
+PATH_TO_INPUT="/home/aggeliki/apoikia/EIGENSTRAT/new_dataset_28_07"
+PATH_TO_OUTPUT="/home/aggeliki/apoikia/admixture_logs"
+
+## convertf does not print map file correctly
+# mv $PATH_TO_INPUT/apoikia.1240K.APOIKIA.map $PATH_TO_INPUT/apoikia.1240K.APOIKIA.map.bak
+# cat $PATH_TO_INPUT/apoikia.1240K.APOIKIA.map.bak | awk 'BEGIN {OFS="\t"}; { print  $1, $2, $3, $4 }' > $PATH_TO_INPUT/apoikia.1240K.APOIKIA.map
+
+# admixture32 $PATH_TO_INPUT/apoikia.1240K.APOIKIA.ped 2 --seed time -j4 > test_file
+
+## 64bit admixture reports segmentation fault
+## 32 bit doesn't
+
+# * Cross Validation for choosing the best K
+for K in 1 2 3 4 5 6 7 8 9 10 11 12; do
+    admixture32 --cv $PATH_TO_INPUT/apoikia.1240K.APOIKIA.bed $K --seed time -j10 | tee $PATH_TO_OUTPUT/apoikia.1240K.no_trim.admixture.${K}.out;
+done
